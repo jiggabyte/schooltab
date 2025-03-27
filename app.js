e504204585b964bd5103b5e55991f89d88531d42
Jigga Byte <jigga.e10@gmail.com>
2025-03-27 23:37:37 +0100

    initial commit

--- app.js
+++ app.js
@@ -19,6 +19,22 @@ app
     })
     .use('/', require('./routes'));
 
+app.use((req, res, next) => {
+    const err = new Error("not found!");
+    err.status = 404;
+    next(err);
+}
+);
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    });
+});
 
 mongodb.initDb((err, mongodb) => {
     if (err) {
