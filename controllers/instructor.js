5fd9bb01178e48889c57f459650f039e54c61d0a
Jigga Byte <jigga.e10@gmail.com>
2025-03-29 14:09:11 +0100

    added try catch blocks for errors routes

--- controllers/instructor.js
+++ controllers/instructor.js
@@ -3,66 +3,91 @@ const ObjectId = require('mongodb').ObjectId;
 
 
 const getAllInstructors = async (req, res, next) => {
-    const result = await mongodb.getDb().db().collection('instructors').find();
-    result.toArray().then((data) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(data);
-    });
-};
+    try {
+        const result = await mongodb.getDb().db().collection('instructors').find();
+        result.toArray().then((data) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(data);
+        });
+    } catch (error) {
+        console.error("Error fetching instructors:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
+}
 
 const getInstructor = async (req, res, next) => {
-    const result = await mongodb.getDb().db().collection('instructors').find({ _id: new ObjectId(req.params.inst_id) });
-    result.toArray().then((data) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(data);
-    });
+    try {
+        const result = await mongodb.getDb().db().collection('instructors').find({ _id: new ObjectId(req.params.inst_id) });
+        result.toArray().then((data) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(data);
+        });
+    } catch (error) {
+        console.error("Error fetching instructors:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 };
 
 const addInstructor = async (req, res, next) => {
-    const user = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        coursesTaught: req.body.coursesTaught,
-        yearOfEmployment: req.body.yearOfEmployment
-    }
-    const result = await mongodb.getDb().db().collection('instructors').insertOne(user);
-    console.log(result);
-    if (result.acknowledged) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(201).json({ Inst_id: result.insertedId });
-    } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(500).json({ error: "Instructor not created!" });
-    }
+    try {
+        const user = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            coursesTaught: req.body.coursesTaught,
+            yearOfEmployment: req.body.yearOfEmployment
+        }
+        const result = await mongodb.getDb().db().collection('instructors').insertOne(user);
+        console.log(result);
+        if (result.acknowledged) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(201).json({ Inst_id: result.insertedId });
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(500).json({ error: "Instructor not created!" });
+        }
+    } catch (error) {
+        console.error("Error fetching instructors:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 }
 
 const updateInstructor = async (req, res, next) => {
-    const student = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        yearOfEmployment: req.body.yearOfEmployment
-    }
-    const result = await mongodb.getDb().db().collection('instructors').updateOne({ _id: new ObjectId(req.params.inst_id) }, { $set: student });
-    console.log(result);
-    if (result.modifiedCount > 0) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json({ success: "Instructor updated!" });
-    } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(500).json({ error: "Instructor not updated!" });
-    }
+    try {
+        const student = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            yearOfEmployment: req.body.yearOfEmployment
+        }
+        const result = await mongodb.getDb().db().collection('instructors').updateOne({ _id: new ObjectId(req.params.inst_id) }, { $set: student });
+        console.log(result);
+        if (result.modifiedCount > 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json({ success: "Instructor updated!" });
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(500).json({ error: "Instructor not updated!" });
+        }
+    } catch (error) {
+        console.error("Error fetching instructors:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 }
 
 const deleteInstructor = async (req, res, next) => {
-    const result = await mongodb.getDb().db().collection('instructors').deleteOne({ _id: new ObjectId(req.params.inst_id) });
-    if (result.deletedCount > 0) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(204).json({ success: "Instructor deleted!" });
-    } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(500).json({ error: "Instructor not deleted!" });
-    }
+    try {
+        const result = await mongodb.getDb().db().collection('instructors').deleteOne({ _id: new ObjectId(req.params.inst_id) });
+        if (result.deletedCount > 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(204).json({ success: "Instructor deleted!" });
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(500).json({ error: "Instructor not deleted!" });
+        }
+    } catch (error) {
+        console.error("Error fetching instructors:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 
 }
 
--- controllers/student.js
+++ controllers/student.js
@@ -3,69 +3,95 @@ const ObjectId = require('mongodb').ObjectId;
 
 
 const getAllStudents = async (req, res, next) => {
-    const result = await mongodb.getDb().db().collection('students').find();
-    result.toArray().then((data) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(data);
-    });
+    try {
+        const result = await mongodb.getDb().db().collection('students').find();
+        result.toArray().then((data) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(data);
+        });
+    } catch (error) {
+        console.error("Error fetching instructors:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 };
 
 const getStudent = async (req, res, next) => {
-    const result = await mongodb.getDb().db().collection('students').find({ _id: new ObjectId(req.params.stud_id) });
-    result.toArray().then((data) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(data);
-    });
+    try {
+        const result = await mongodb.getDb().db().collection('students').find({ _id: new ObjectId(req.params.stud_id) });
+        //  throw new Error("Error fetching student");
+        result.toArray().then((data) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(data);
+        });
+    } catch (error) {
+        console.error("Error fetching instructors:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 };
 
 const addStudent = async (req, res, next) => {
-    const user = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        yearOfAdmission: req.body.yearOfAdmission,
-        courseOfStudy: req.body.courseOfStudy,
-        department: req.body.department,
-        level: req.body.level
-    }
-    const result = await mongodb.getDb().db().collection('students').insertOne(user);
-    console.log(result);
-    if (result.acknowledged) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(201).json({ stud_id: result.insertedId });
-    } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(500).json({ error: "Student not created!" });
-    }
+    try {
+        const user = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            yearOfAdmission: req.body.yearOfAdmission,
+            courseOfStudy: req.body.courseOfStudy,
+            department: req.body.department,
+            level: req.body.level
+        }
+        const result = await mongodb.getDb().db().collection('students').insertOne(user);
+        console.log(result);
+        if (result.acknowledged) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(201).json({ stud_id: result.insertedId });
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(500).json({ error: "Student not created!" });
+        }
+    } catch (error) {
+        console.error("Error fetching instructors:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 }
 
 const updateStudent = async (req, res, next) => {
-    const student = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        courseOfStudy: req.body.courseOfStudy,
-        yearOfAdmission: req.body.yearOfAdmission
-    }
-    const result = await mongodb.getDb().db().collection('students').updateOne({ _id: new ObjectId(req.params.stud_id) }, { $set: student });
-    console.log(result);
-    if (result.modifiedCount > 0) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json({ success: "Student updated!" });
-    } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(500).json({ error: "Student not updated!" });
-    }
+    try {
+        const student = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            courseOfStudy: req.body.courseOfStudy,
+            yearOfAdmission: req.body.yearOfAdmission
+        }
+        const result = await mongodb.getDb().db().collection('students').updateOne({ _id: new ObjectId(req.params.stud_id) }, { $set: student });
+        console.log(result);
+        if (result.modifiedCount > 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json({ success: "Student updated!" });
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(500).json({ error: "Student not updated!" });
+        }
+    } catch (error) {
+        console.error("Error fetching instructors:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 }
 
 const deleteStudent = async (req, res, next) => {
-    const result = await mongodb.getDb().db().collection('students').deleteOne({ _id: new ObjectId(req.params.stud_id) });
-    if (result.deletedCount > 0) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(204).json({ success: "Student deleted!" });
-    } else {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(500).json({ error: "Student not deleted!" });
-    }
+    try {
+        const result = await mongodb.getDb().db().collection('students').deleteOne({ _id: new ObjectId(req.params.stud_id) });
+        if (result.deletedCount > 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(204).json({ success: "Student deleted!" });
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(500).json({ error: "Student not deleted!" });
+        }
+    } catch (error) {
+        console.error("Error fetching instructors:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    };
 
 }
 
